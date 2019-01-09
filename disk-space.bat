73961acbd5100b3b29407a402c52bb0cdc846576
Ghanshyam Dusane <37286886+ghanshyamdusane@users.noreply.github.com>
2019-01-10 00:36:42 +0530

    Add files via upload

---
+++ disk-space.bat
@@ -0,0 +1,3 @@
+Powershell.exe C:\aws_scripts\mon-put-metrics-disk.ps1 -aws_credential_file C:\aws_scripts\awscreds.conf -disk_drive C:, D: -disk_space_used -disk_space_units gigabytes -logfile C:\mylogfile.log
+
+
---
+++ memory.bat
@@ -0,0 +1,3 @@
+Powershell.exe C:\aws_scripts\mon-put-metrics-mem.ps1 -aws_credential_file C:\aws_scripts\awscreds.conf -mem_util -memory_units gigabytes -from_scheduler -logfile C:\mylogfile.log
+
+
